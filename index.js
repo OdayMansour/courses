9abf5a55a3c6d120960a2b805f163658f2e5b641
Oday Mansour <odaym@nsour.net>
2017-06-30 17:08:52 +0200

    Added code to return complete state JSON for list.html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const pool = require('./lib/db');
+const pool = require('./lib/db')
  
 // Runs a query and processes results
 function queryAndProcess(qtext, qvalues, processor, args) {
@@ -15,12 +15,15 @@ function queryAndProcess(qtext, qvalues, processor, args) {
   });
 }
 
-
-
 // Processor stuff
 // Processor that just sends results down the line
-function justsend(rows, res) {
-  res.send(JSON.stringify(rows));
+function sendAsJSON(rows, res) {
+  sendAsPrettyJSON(rows, res)
+}
+
+function sendAsPrettyJSON(rows, res) {
+  res.type('application/json')
+  res.send(JSON.stringify(rows, null, 2))
 }
 
 
@@ -34,23 +37,23 @@ function nothing(req, res) {
 
 // User stuff functions
 function allUsers(req, res) {
-  queryAndProcess('SELECT * FROM users WHERE $1', ['TRUE'], justsend, res)
+  queryAndProcess('SELECT * FROM users WHERE $1', ['TRUE'], sendAsJSON, res)
 }
 
 function oneUserFromUsername(req, res, username) {
-  queryAndProcess('SELECT * FROM users WHERE username = $1', [username], justsend, res)
+  queryAndProcess('SELECT * FROM users WHERE username = $1', [username], sendAsJSON, res)
 }
 
 function oneUserFromUserid(req, res, userid) {
-  queryAndProcess('SELECT * FROM users WHERE id = $1', [userid], justsend, res)
+  queryAndProcess('SELECT * FROM users WHERE id = $1', [userid], sendAsJSON, res)
 }
 
 function householdUsers(req, res, householdid) {
-  queryAndProcess('SELECT * FROM users WHERE id IN (SELECT userid FROM user_household WHERE householdid = $1)', [householdid], justsend, res)
+  queryAndProcess('SELECT * FROM users WHERE id IN (SELECT userid FROM user_household WHERE householdid = $1)', [householdid], sendAsJSON, res)
 }
 
 function listUser(req, res, listid) {
-  queryAndProcess('SELECT * FROM users WHERE id = (SELECT userid FROM lists WHERE id = $1)', [listid], justsend, res)
+  queryAndProcess('SELECT * FROM users WHERE id = (SELECT userid FROM lists WHERE id = $1)', [listid], sendAsJSON, res)
 }
 
 function getUser(req, res) {
@@ -79,19 +82,19 @@ function getUser(req, res) {
 
 // List stuff functions
 function allLists(req, res) {
-  queryAndProcess('SELECT * FROM lists WHERE $1', ['TRUE'], justsend, res)
+  queryAndProcess('SELECT * FROM lists WHERE $1', ['TRUE'], sendAsJSON, res)
 }
 
 function oneList(req, res, listid) {
-  queryAndProcess('SELECT * FROM lists WHERE id = $1', [listid], justsend, res)
+  queryAndProcess('SELECT * FROM lists WHERE id = $1', [listid], sendAsJSON, res)
 }
 
 function userLists(req, res, userid) {
-  queryAndProcess('SELECT * FROM lists WHERE userid = $1', [userid], justsend, res)
+  queryAndProcess('SELECT * FROM lists WHERE userid = $1', [userid], sendAsJSON, res)
 }
 
 function householdLists(req, res, householdid) {
-  queryAndProcess('SELECT * FROM lists WHERE userid IN (select userid from user_household WHERE householdid = $1)', [householdid], justsend, res)
+  queryAndProcess('SELECT * FROM lists WHERE userid IN (select userid from user_household WHERE householdid = $1)', [householdid], sendAsJSON, res)
 }
 
 function getList(req, res) {
@@ -113,6 +116,64 @@ function getList(req, res) {
 
 }
 
+// DB stuff helpers
+function parametrize(arr) {
+  var params = [];
+    for(var i = 1; i <= arr.length; i++) {
+      params.push('$' + i);
+    }
+  return params
+}
+
+function col(colname) {
+  return function(x){return x[colname]}
+}
+
+function addItemsToLists(rows_listid_itemname, args) {
+  var rows_listid_listname = args[0]
+  var res = args[1]
+  for (var i=0; i<rows_listid_listname.length; i++) {
+    rows_listid_listname[i]['items'] = rows_listid_itemname.filter(function(x){return x.id == rows_listid_listname[i].id}).map(col('name'))
+  }
+  sendAsJSON({"list" : rows_listid_listname}, res)
+}
+
+function buildLists(rows_listid_listname, res) {
+  // We just got fed an array of { id, name } refering to lists
+  if (rows_listid_listname.length > 0) {
+    queryAndProcess(
+      `
+        select 
+          lists.id, 
+          items.name 
+        from 
+          lists 
+          join list_item on lists.id = list_item.listid 
+          join items on list_item.itemid = items.id 
+        where 
+          lists.id in (` + parametrize(rows_listid_listname) + `);`,
+      rows_listid_listname.map(col('id')), 
+      addItemsToLists,
+      [rows_listid_listname, res]
+      )
+  } else {
+    sendAsJSON({}, res)
+  }
+}
+
+function getFullLists(req, res) {
+  console.log("On /fulllists")
+  console.log(JSON.stringify(req.query))
+
+  if (typeof req.query.userid != 'undefined') {
+    queryAndProcess(
+      'select lists.id, lists.name from lists where lists.userid = $1',
+      [req.query.userid],
+      buildLists,
+      res)
+  }
+}
+
 app.get('/', nothing)
 
 // User stuff
@@ -120,6 +181,10 @@ app.get('/user', getUser)
 
 // List stuff
 app.get('/list', getList)
+
+// Lists with full details
+app.get('/fulllists', getFullLists)
+
 
 // Item stuff
 // app.get('/items', allItems)
