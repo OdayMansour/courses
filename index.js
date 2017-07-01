74da0348b897993926c42fb84cc967009235f46e
Oday Mansour <odaym@nsour.net>
2017-07-01 15:21:29 +0200

    Rewrote and reorganized functions. Making use of Express Routers now, much cleaner

--- index.js
+++ index.js
@@ -1,194 +1,54 @@
+// Not mine
 const express = require('express')
 const app = express()
 
+// Mine
 const pool = require('./lib/db')
- 
-// Runs a query and processes results
-function queryAndProcess(qtext, qvalues, processor, args) {
-  pool.query(qtext, qvalues, function(err, result) {
-    if(err) {
-      console.error('error running query', err);
-      return []
-    }
-    // return sender.send(JSON.stringify(result.rows));
-    return processor(result.rows, args);
-  });
-}
+const processor = require('./lib/processor')
+const users = require('./lib/users')
+const lists = require('./lib/lists')
+const items = require('./lib/items')
+const households = require('./lib/households')
 
-// Processor stuff
-// Processor that just sends results down the line
-function sendAsJSON(rows, res) {
-  sendAsPrettyJSON(rows, res)
-}
+// Paths
+app.get('/', nothing)
 
-function sendAsPrettyJSON(rows, res) {
-  res.type('application/json')
-  res.send(JSON.stringify(rows, null, 2))
-}
+// Routers
+var routerUsers = express.Router()
+var routerLists = express.Router()
+var routerItems = express.Router()
+var routerHouseholds = express.Router()
 
+// Assigning routers
+app.use('/users', routerUsers)
+app.use('/lists', routerLists)
+app.use('/items', routerItems)
+app.use('/households', routerHouseholds)
+
+// Users stuff
+routerUsers.get('/:userid', users.userFromUserid)
+routerUsers.get('/:userid/lists', users.listsFromUserid)
+routerUsers.get('/:userid/lists/full', users.fullListsFromUserid)
+routerUsers.get('/:userid/households', users.householdsFromUserid)
+
+// Lists stuff
+routerLists.get('/:listid', lists.listFromListid)
+routerLists.get('/:listid/full', lists.fullListFromListid)
+
+// Item stuff
+routerItems.get('/:itemid', items.itemFromItemid)
+
+// Households stuff
+routerHouseholds.get('/:householdid', households.householdFromHouseholdid)
+routerHouseholds.get('/:householdid/users', households.usersFromHouseholdid)
+routerHouseholds.get('/:householdid/lists', households.listsFromHouseholdid)
+routerHouseholds.get('/:householdid/lists/full', households.fullListsFromHouseholdid)
 
 
 // Generic stuff
 function nothing(req, res) {
   res.send("Nothing to see here.")
 }
-
-
-
-// User stuff functions
-function allUsers(req, res) {
-  queryAndProcess('SELECT * FROM users WHERE $1', ['TRUE'], sendAsJSON, res)
-}
-
-function oneUserFromUsername(req, res, username) {
-  queryAndProcess('SELECT * FROM users WHERE username = $1', [username], sendAsJSON, res)
-}
-
-function oneUserFromUserid(req, res, userid) {
-  queryAndProcess('SELECT * FROM users WHERE id = $1', [userid], sendAsJSON, res)
-}
-
-function householdUsers(req, res, householdid) {
-  queryAndProcess('SELECT * FROM users WHERE id IN (SELECT userid FROM user_household WHERE householdid = $1)', [householdid], sendAsJSON, res)
-}
-
-function listUser(req, res, listid) {
-  queryAndProcess('SELECT * FROM users WHERE id = (SELECT userid FROM lists WHERE id = $1)', [listid], sendAsJSON, res)
-}
-
-function getUser(req, res) {
-  console.log("On /list")
-  console.log(JSON.stringify(req.query))
-
-  if (typeof req.query.username != 'undefined') {
-    oneUserFromUsername(req, res, req.query.username)
-  
-  } else if (typeof req.query.userid != 'undefined') {
-    oneUserFromUserid(req, res, req.query.userid)
-  
-  } else if (typeof req.query.householdid != 'undefined') {
-    householdUsers(req, res, req.query.householdid)
-  
-  } else if (typeof req.query.listid != 'undefined') {
-    listUser(req, res, req.query.listid)
-  
-  } else {
-    allUsers(req, res)    
-  }
-
-}
-
-
-
-// List stuff functions
-function allLists(req, res) {
-  queryAndProcess('SELECT * FROM lists WHERE $1', ['TRUE'], sendAsJSON, res)
-}
-
-function oneList(req, res, listid) {
-  queryAndProcess('SELECT * FROM lists WHERE id = $1', [listid], sendAsJSON, res)
-}
-
-function userLists(req, res, userid) {
-  queryAndProcess('SELECT * FROM lists WHERE userid = $1', [userid], sendAsJSON, res)
-}
-
-function householdLists(req, res, householdid) {
-  queryAndProcess('SELECT * FROM lists WHERE userid IN (select userid from user_household WHERE householdid = $1)', [householdid], sendAsJSON, res)
-}
-
-function getList(req, res) {
-  console.log("On /list")
-  console.log(JSON.stringify(req.query))
-
-  if (typeof req.query.listid != 'undefined') {
-    oneList(req, res, req.query.listid)
-  
-  } else if (typeof req.query.userid != 'undefined') {
-    userLists(req, res, req.query.userid)
-  
-  } else if (typeof req.query.householdid != 'undefined') {
-    householdLists(req, res, req.query.householdid)
-  
-  } else {
-    allLists(req, res)    
-  }
-
-}
-
-// DB stuff helpers
-function parametrize(arr) {
-  var params = [];
-    for(var i = 1; i <= arr.length; i++) {
-      params.push('$' + i);
-    }
-  return params
-}
-
-function col(colname) {
-  return function(x){return x[colname]}
-}
-
-function addItemsToLists(rows_listid_itemname, args) {
-  var rows_listid_listname = args[0]
-  var res = args[1]
-  for (var i=0; i<rows_listid_listname.length; i++) {
-    rows_listid_listname[i]['items'] = rows_listid_itemname.filter(function(x){return x.id == rows_listid_listname[i].id}).map(col('name'))
-  }
-  sendAsJSON({"list" : rows_listid_listname}, res)
-}
-
-function buildLists(rows_listid_listname, res) {
-  // We just got fed an array of { id, name } refering to lists
-  if (rows_listid_listname.length > 0) {
-    queryAndProcess(
-      `
-        select 
-          lists.id, 
-          items.name 
-        from 
-          lists 
-          join list_item on lists.id = list_item.listid 
-          join items on list_item.itemid = items.id 
-        where 
-          lists.id in (` + parametrize(rows_listid_listname) + `);`,
-      rows_listid_listname.map(col('id')), 
-      addItemsToLists,
-      [rows_listid_listname, res]
-      )
-  } else {
-    sendAsJSON({}, res)
-  }
-}
-
-function getFullLists(req, res) {
-  console.log("On /fulllists")
-  console.log(JSON.stringify(req.query))
-
-  if (typeof req.query.userid != 'undefined') {
-    queryAndProcess(
-      'select lists.id, lists.name from lists where lists.userid = $1',
-      [req.query.userid],
-      buildLists,
-      res)
-  }
-}
-
-app.get('/', nothing)
-
-// User stuff
-app.get('/user', getUser)
-
-// List stuff
-app.get('/list', getList)
-
-// Lists with full details
-app.get('/fulllists', getFullLists)
-
-
-// Item stuff
-// app.get('/items', allItems)
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
